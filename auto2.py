6f01bca212c3653de27f05497ee6c84dca1873d3
oliver.pulk <oliver.pulk@vikk.ee>
2025-03-28 10:44:52 +0200

    pygame

--- auto2.py
+++ auto2.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
 
-# värvid
+# Värvid
 IGreen = [153, 255, 153]
 Black = [0, 0, 0]
 
@@ -19,6 +19,13 @@ punane_auto = pygame.image.load("f1_red.png")
 sinine_auto = pygame.image.load("f1_blue.png")
 sinine_auto2 = pygame.image.load("f1_blue.png")
 sinine_auto3 = pygame.image.load("f1_blue.png")
+
+# Mängija auto algasend
+player_x = 300
+player_y = 390
+player_speed = 0.1
+
+# Vastaste autod
 PosX, PosY = 180, 0
 PosX2, PosY2 = 410, 200
 PosX3, PosY3 = 300, 120
@@ -32,27 +39,41 @@ score = 0
 
 gameover = False
 
-# Mängu sulgemine ristist
+# Mängu tsükkel
 while not gameover:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameover = True
 
+    # Klaviatuuri sisendi kontroll
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:   # Kui vajutatakse vasakule noolt
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT]:  # Kui vajutatakse paremale noolt
+        player_x += player_speed
 
+    # Veendume, et auto ei läheks ekraanilt välja
+    if player_x < 0:
+        player_x = 0
+    if player_x > 640 - punane_auto.get_width():
+        player_x = 640 - punane_auto.get_width()
 
+    # Taust ja autod
     screen.blit(Rally, [0,0])
-    screen.blit(punane_auto, (300, 390))
+    screen.blit(punane_auto, (player_x, player_y))
     screen.blit(sinine_auto, (PosX, PosY))
     screen.blit(sinine_auto2, (PosX2, PosY2))
     screen.blit(sinine_auto3, (PosX3, PosY3))
 
+    # Vastaste liikumine
     PosY += speed
     PosY2 += speed2
     PosY3 += speed3
 
+    # Kui autod jõuavad alla, alustavad uuesti ülevalt ja lisatakse skoori
     if PosY > 480:
         PosY = -sinine_auto.get_height()
-        score += 1  # Lisame skoorile 1 punkti
+        score += 1
 
     if PosY2 > 480:
         PosY2 = -sinine_auto2.get_height()
@@ -62,11 +83,9 @@ while not gameover:
         PosY3 = -sinine_auto3.get_height()
         score += 1
 
-    # Teksti loomine ja kuvamine
+    # Skoori kuvamine
     score_text = font.render("Skoor: " + str(score), True, Black)
     screen.blit(score_text, (10, 10))
-
-
 
     pygame.display.flip()
 
